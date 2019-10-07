40229ef0f916f9fd8a806807a41040ad5a229df1
Monica R <mlisareyes@gmail.com>
2019-10-07 05:10:46 +0000

    adding files

---
+++ lib/desserts.rb
@@ -0,0 +1,14 @@
+### REQUIRED GEMS ###
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+### NAMESPACE MODULE ###
+module FrenchDesserts
+end
+
+### REQUIRED FILES ###
+require_relative "./FrenchDesserts/version"
+require_relative "./FrenchDesserts/dessert-descriptionß"
+require_relative "./FrenchDesserts/scraper"
+require_relative "./FrenchDesserts/cli"
