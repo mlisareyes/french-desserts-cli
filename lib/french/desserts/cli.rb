d259aa0c024fb7818c2cbdcb6a5139fb41ca97de
Monica R <mlisareyes@gmail.com>
2019-10-07 04:35:46 +0000

    first commit

---
+++ lib/french/desserts/cli.rb
@@ -0,0 +1,10 @@
+require "french/desserts/cli/version"
+
+module French
+  module Desserts
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/french/desserts/cli/version.rb
@@ -0,0 +1,7 @@
+module French
+  module Desserts
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end
