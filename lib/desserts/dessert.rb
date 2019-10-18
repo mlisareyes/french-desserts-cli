06dfa61f6edc2befa9a315aba5caab3e01d892aa
Monica R <mlisareyes@gmail.com>
2019-10-18 03:26:13 +0000

    fixed order of the methods in the dessert file

--- lib/desserts/dessert.rb
+++ lib/desserts/dessert.rb
@@ -7,13 +7,6 @@ class Desserts::Dessert
     @description = description
   end
 
-  def self.all
-    @@all = self.scrape_desserts.collect do |dessert| #this creates a new array of dessert objects taken from the scraped dessert hash
-      self.new(dessert)
-    end
-  end
-
-
   def self.scrape_desserts
     doc = Nokogiri::HTML(open("https://www.epicurious.com/recipes-menus/easy-french-desserts-quick-simple-recipes-gallery"))
       doc.css("div.gallery-slide-caption").collect do |dessert|
@@ -24,9 +17,14 @@ class Desserts::Dessert
     end
   end
 
-  def self.test
-
-    #binding.pry
+  def self.all
+    @@all = self.scrape_desserts.collect do |dessert| #this creates a new array of dessert objects taken from the scraped dessert hash
+      self.new(dessert)
+    end
   end
+
+  #def self.test
+    #binding.pry
+  #end
 
 end
